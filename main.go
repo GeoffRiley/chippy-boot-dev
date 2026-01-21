d57081dbb73b09345db3d2facd6821f73a42607d
Geoff Riley <geoffr@adaso.com>
2026-01-21 23:35:42 +0000

    refactor: Integrate new database, API handlers, and routes in main.go

--- main.go
+++ main.go
@@ -1,110 +1,39 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"database/sql"
 	"log"
 	"net/http"
-	"regexp"
+	"os"
 	"sync/atomic"
+
+	"github.com/GeoffRiley/chippy-boot-dev/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
-
-type apiConfig struct {
-	fileserverHits atomic.Int32
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/html")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf(`
-<html>
-
-<body>
-	<h1>Welcome, Chirpy Admin</h1>
-	<p>Chirpy has been visited %d times!</p>
-</body>
-
-</html>
-	`, cfg.fileserverHits.Load())))
-}
-
-func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-	//if cfg.platform != "dev" {
-	//	w.WriteHeader(http.StatusForbidden)
-	//	w.Write([]byte("Reset is only allowed in dev environment."))
-	//	return
-	//}
-
-	cfg.fileserverHits.Store(0)
-	//cfg.db.Reset(r.Context())
-	w.WriteHeader(http.StatusOK)
-	//w.Write([]byte("Hits reset to 0 and database reset to initial state."))
-	w.Write([]byte("Hits reset to 0."))
-}
-
-type chirpRequest struct {
-	Body string `json:"body"`
-}
-
-type errorResponse struct {
-	Error string `json:"error"`
-}
-
-type validResponse struct {
-	ValidBody bool `json:"valid"`
-}
-
-type cleanedResponse struct {
-	CleanedBody string `json:"cleaned_body"`
-}
-
-var profaneWords = []string{"kerfuffle", "sharbert", "fornax"}
-
-func cleanProfanity(body string) string {
-	for _, word := range profaneWords {
-		regex := regexp.MustCompile(`\b(?i)` + word + `\b`)
-		body = regex.ReplaceAllString(body, "****")
-	}
-	return body
-}
-
-func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
-	var chirp chirpRequest
-
-	if err := json.NewDecoder(r.Body).Decode(&chirp); err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(errorResponse{Error: "Invalid JSON"})
-		return
-	}
-
-	if len(chirp.Body) > 140 {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(errorResponse{Error: "Chirp is too long"})
-		return
-	}
-
-	cleanedBody := cleanProfanity(chirp.Body)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(cleanedResponse{CleanedBody: cleanedBody})
-	//json.NewEncoder(w).Encode(validResponse{ValidBody: true})
-}
 
 func main() {
 	const port = "8080"
+	godotenv.Load(".env")
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening database: %s", err)
+	}
+	dbQueries := database.New(dbConn)
+
+	platform := os.Getenv("PLATFORM")
+	if platform == "" {
+		log.Fatal("PLATFORM environment variable is not set")
+	}
 
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		db:             dbQueries,
+		platform:       platform,
 	}
 
 	mux := http.NewServeMux()
@@ -119,7 +48,10 @@ func main() {
 	fsHandlerAssets := apiCfg.middlewareMetricsInc(http.StripPrefix("/app/assets/", http.FileServer(http.Dir("./assets"))))
 	mux.Handle("/app/assets/", fsHandlerAssets)
 
-	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
+	mux.HandleFunc("POST /api/chirps", apiCfg.handlerPostChirps)
+	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
+	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerGetChirp)
+	mux.HandleFunc("GET /api/chirps", apiCfg.handlerGetChirps)
 
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
