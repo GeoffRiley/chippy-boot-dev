f7ace5d5ce5b41e83deed62d54f4fb4a9d24937b
Geoff Riley <geoffr@adaso.com>
2026-01-21 23:35:41 +0000

    refactor: Define shared application structures

---
+++ structures.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"sync/atomic"
+	"time"
+
+	"github.com/GeoffRiley/chippy-boot-dev/internal/database"
+	"github.com/google/uuid"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+	db             *database.Queries
+	platform       string
+}
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
+
+type chirpRequest struct {
+	Body string `json:"body"`
+}
+
+type errorResponse struct {
+	Error string `json:"error"`
+}
+
+type validResponse struct {
+	ValidBody bool `json:"valid"`
+}
+
+type cleanedResponse struct {
+	CleanedBody string `json:"cleaned_body"`
+}
+
+type Chirp struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	UserID    uuid.UUID `json:"user_id"`
+	Body      string    `json:"body"`
+}
